9fbdc29049b4b444560d46923afcbba9c4ec8d4b
Olivier YOUF <olivier.youf@axa.fr>
2020-11-14 00:28:23 +0100

    feat:add logout button

--- src/shared/components/UserCard.jsx
+++ src/shared/components/UserCard.jsx
@@ -1,8 +1,8 @@
-import { Card, Button } from "antd";
-import { LoginOutlined } from "@ant-design/icons";
+import { Card, Button } from 'antd';
+import { LoginOutlined } from '@ant-design/icons';
 
-import jwtBadge from "shared/images/logo-jwt.svg";
-import userImage from "shared/images/user.svg";
+import jwtBadge from 'shared/images/logo-jwt.svg';
+import userImage from 'shared/images/user.svg';
 
 const More = ({ token }) => (
   <a href={`https://jwt.io?id_token=${token}`} className="user-token">
@@ -10,21 +10,19 @@ const More = ({ token }) => (
   </a>
 );
 
-const UserCard = ({ user, onLogin }) => {
+const UserCard = ({ user, onLogin, onLogout }) => {
   return user ? (
     <Card title="YOUR ID TOKEN" extra={<More token={user.id_token} />} className="user-card">
       <img src={user?.profile?.picture || userImage} alt="user avatar" className="user-avatar" />
       <p className="user-mail">{user.profile.email}</p>
+      {onLogout && (
+        <Button type="primary" shape="round" className="button-logout" size="large" onClick={onLogout}>
+          Logout
+        </Button>
+      )}
     </Card>
   ) : (
-    <Button
-      type="primary"
-      shape="round"
-      icon={<LoginOutlined />}
-      className="button-summit"
-      size="large"
-      onClick={onLogin}
-    >
+    <Button type="primary" shape="round" icon={<LoginOutlined />} className="button-summit" size="large" onClick={onLogin}>
       Login
     </Button>
   );
